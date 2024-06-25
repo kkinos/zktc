1cca42e26bf05bcf3f07390a51edddf6ddaa23df
kinpoko <79001545+kinpoko@users.noreply.github.com>
2024-06-25 23:37:09 +0900

    WIP

--- src/uart_tx.sv
+++ src/uart_tx.sv
@@ -1,5 +1,5 @@
 module uart_tx #(
-    parameter WAIT_CYCLES = 868  // 100M 115200 baud rate
+    parameter WAIT_COUNT = 868  // 100M 115200 baud rate
 ) (
     input logic clk,
     input logic rst,
@@ -10,30 +10,50 @@ module uart_tx #(
     output logic tx
 );
 
-  logic [$clog2(WAIT_CYCLES)-1:0] waitcnt;
-  logic tx_clk;
+  typedef enum {
+    IDLE,
+    SEND
+  } state_t;
+  state_t state;
+
+  logic [$clog2(WAIT_COUNT)-1:0] waitcnt;
+  logic [3:0] bitcnt;
+  logic [9:0] tx_buf;
+  assign tx   = tx_buf[0];
+  assign busy = state != IDLE;
+
   always_ff @(posedge clk) begin
     if (rst) begin
+      state   <= IDLE;
       waitcnt <= 0;
-      tx_clk  <= 0;
+      bitcnt  <= 0;
+      tx_buf  <= 10'h3ff;
     end else begin
-      if (waitcnt == WAIT_CYCLES - 1) begin
-        tx_clk  <= 1;
-        waitcnt <= 0;
-      end else begin
-        tx_clk  <= 0;
-        waitcnt <= waitcnt + 1;
-      end
+      case (state)
+        IDLE: begin
+          if (en) begin
+            state  <= SEND;
+            tx_buf <= {1'b1, tx_data, 1'b0};
+          end
+        end
+        SEND: begin
+          if (waitcnt != WAIT_COUNT) begin
+            waitcnt <= waitcnt + 1;
+          end else begin
+            waitcnt <= 0;
+            if (bitcnt == 9) begin
+              bitcnt <= 0;
+              state  <= IDLE;
+            end else begin
+              bitcnt <= bitcnt + 1;
+              tx_buf <= {1'b1, tx_buf[9:1]};
+            end
+          end
+        end
+      endcase
     end
   end
 
-  typedef enum {
-    START,
-    SEND,
-    STOP,
-    FINISH
-  } state_t;
-  state_t state;
 
 
 endmodule
